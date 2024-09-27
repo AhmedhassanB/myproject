4d3934ae41d92be935f7591f33471fa896b5098c
Ahmed hassan <amh247408@gmail.com>
2024-09-27 09:16:54 +0300

    creat new version

---
+++ newjava.js
@@ -0,0 +1,10 @@
+'use strict'
+let img = document.querySelector('.img1');
+let chang = document.querySelector('.contain');
+
+function changphons(phone) {
+    img.src = phone;
+}
+function changcolor(color) {
+    chang.style.background = color;
+}
